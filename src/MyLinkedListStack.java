2442e93538e9d6cab08db48db09560beba698e6b
pouterr <poutergutorov@gmail.com>
2023-04-29 20:05:49 +0600

    Creating a constructor for the class

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -2,4 +2,8 @@ import java.util.EmptyStackException;
 
 public class MyLinkedListStack<T> {
     private MyLinkedList<T> list;
+
+    public MyLinkedListStack() {
+        list = new MyLinkedList<>();
+    }
 }
