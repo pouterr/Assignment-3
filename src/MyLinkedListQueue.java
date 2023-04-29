d97f8aa222aea3c63f2284edeeecfa22147550cd
pouterr <poutergutorov@gmail.com>
2023-04-30 00:46:11 +0600

    Creating a constructor for the class

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -2,4 +2,11 @@ import java.util.NoSuchElementException;
 
 public class MyLinkedListQueue<T> {
     private MyLinkedList<T> list;
+
+    /**
+     * Constructs an empty queue.
+     */
+    public MyLinkedListQueue() {
+        list = new MyLinkedList<>();
+    }
 }
