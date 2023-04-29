bc81f1f77b6d5feb4a3edb456db04ff787a1efc4
pouterr <poutergutorov@gmail.com>
2023-04-30 01:05:13 +0600

    Implementing the peek() method

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -35,6 +35,19 @@ public class MyLinkedListQueue<T> {
     }
 
     /**
+     * Returns the front element of the queue without removing it.
+     *
+     * @return the front element of the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return list.get(0);
+    }
+
+    /**
      * Returns true if the queue is empty, false otherwise.
      *
      * @return true if the queue is empty, false otherwise
