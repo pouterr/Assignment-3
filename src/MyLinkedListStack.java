28a4b03b3cf491a636d1e653743a48f32a45e4e0
pouterr <poutergutorov@gmail.com>
2023-04-30 00:32:54 +0600

    implementing isEmpty method

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -10,4 +10,13 @@ public class MyLinkedListStack<T> {
     public void push(T element) {
         list.add(element);
     }
+
+    public boolean isEmpty() {
+        int size = list.size();
+        if(size ==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
